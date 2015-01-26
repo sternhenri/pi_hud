7032e71e7d11fc3858581cc42006ea625f745006
Henri S <sternhenri2@gmail.com>
2015-01-26 00:48:10 -0500

    up v1

--- display_shouts.py
+++ display_shouts.py
@@ -3,6 +3,8 @@ from flask import Flask
 import time
 import RPi.GPIO as io
 
+app = Flask(__name__)
+
 def setup_hardware():
 	io.setmode(io.BCM)
 
@@ -19,22 +21,34 @@ def turn_off_lights():
 	io.output(yellowPin, off)
 	io.output(bluePin, off)
 	io.output(greenPin, off)
-while True:
-	io.output(yellowPin, False)
-	time.sleep(1)
-	io.output(yellowPin, True)
-	io.output(bluePin, False)
-	time.sleep(1)
-	io.output(bluePin, True)
-	io.output(greenPin, False)
-	time.sleep(1)
-	io.output(greenPin, True)
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+def switch_light(color, switch_type):
+	pin = -1
+	if color == 'yellow':
+		pin = yellowPin
+	elif color == 'blue':
+		pin = bluePin
+	elif color == 'green':
+		pin = greenPin
+
+	io.output(pin, switch_type)
+
+@app.route('/shout_display/<event>/<data>')
+def parse_event(event, data):
+    if event == 'post':
+    	color = 'yellow'
+    elif event == 'signup':
+    	color = 'blue'
+    elif event == 'sale':
+    	color = 'green'
+
+    switch_light(color, on)
+    time.sleep(3)
+    switch_light(color, off)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	setup_hardware()
+	turn_off_lights()
+    app.run()
 
